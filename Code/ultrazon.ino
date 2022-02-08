f943745bb35ab8660bb7ebc10a51a2ede52c55c0
dsprz <84564775+dsprz@users.noreply.github.com>
2022-02-08 19:37:18 +0100

    Add files via upload

--- Code/ultrazon.ino
+++ Code/ultrazon.ino
@@ -7,6 +7,9 @@ const int trig = 10;
 const int buzzer = 9;
 const int echo = 8;
 float distance = 0.0;
+int pause;
+
+NewPing sonar(trig, echo, 200);
 TonePlayer tone1 (TCCR1A, TCCR1B, OCR1AH, OCR1AL, TCNT1H, TCNT1L);
 void setup() {
   Serial.begin(9600);
@@ -16,8 +19,13 @@ void setup() {
 }
 
 void loop() {
-  NewPing sonar(trig, echo, 200);
   distance = sonar.ping_cm();
   Serial.println(distance);
-  tone1.tone(220);
+  if(distance < 40 & distance >=30){
+    tone1.tone(400);
+    }
+  if (distance < 30){
+    tone1.tone(600);
+  }
+  if (distance>=40){tone1.noTone();}
 }
